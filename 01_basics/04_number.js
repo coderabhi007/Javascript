a3ccf2cb0f297613e8bbc9b025a9831eee4b4613
coderabhi007 <124509430+coderabhi007@users.noreply.github.com>
2024-03-20 17:22:14 +0000

    numbers & maths

---
+++ 01_basics/04_number.js
@@ -0,0 +1,8 @@
+//to string
+let n=3.14252
+//console.log(n.toFixed())
+//console.log(n.toPrecision(2))
+//console.log(Math.round(n))
+//console.log(Math.ceil(n))
+//console.log(Math.floor(n))
+console.log(Math.floor(Math.random()*10))
