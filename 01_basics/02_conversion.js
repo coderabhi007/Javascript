2d21acf26ab86424939861e4fd48aa6120882d00
coderabhi007 <124509430+coderabhi007@users.noreply.github.com>
2024-03-20 16:47:59 +0000

    conversion

---
+++ 01_basics/02_conversion.js
@@ -0,0 +1,8 @@
+/*let count="33"
+let number=Boolean(count)
+console.log(count)
+console.log(typeof(count))
+console.log(number)
+console.log(typeof(number))*/
+//console.log(2**3) //used for power
+console.log(null===0)
