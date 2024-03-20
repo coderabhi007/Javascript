bef163604f4d9acf54c9b4a7c4f676aa51eefd6a
coderabhi007 <124509430+coderabhi007@users.noreply.github.com>
2024-03-20 17:09:08 +0000

    string

---
+++ 01_basics/03_string.js
@@ -0,0 +1,11 @@
+//good practice of writting in js
+let name="Abhi"
+let age=20
+//console.log(`My name is:"${name} "and my age:${age}`)
+//console.log(name.toUpperCase())
+//console.log(name.charAt(1))
+//console.log(name.indexOf('b'))
+//console.log(name.substring(1,3))
+//console.log(name.slice(-1,3))
+let c="  hey i am abhi    "
+console.log(c.trim())
